4cfb7d30ee8c7d015f2726213c2382c1f72ccea7
Armin <25596278+arminzukic@users.noreply.github.com>
2022-06-12 22:09:45 +0200

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # K6-Paypal-Api-Load-Testing
 The Load Testing Project of the PayPal API with K6 Tool
+
+- `docker-compose up -d influxdb grafana`
+- Load http://localhost:3000, and import the `grafana_dashboard-v1.json` config to a new dashboard.
+- `docker-compose run k6 run /tests/load-test.js`
---
+++ tests/common.js
@@ -0,0 +1,4 @@
+export const BASE_URL = "https://api-m.sandbox.paypal.com";
+//clientID and clientSecret, default ones given by paypal, it is recommanded to use your own, generated on https://developer.paypal.com/developer
+export const clientID = "AUv8rrc_P-EbP2E0mpb49BV7rFt3Usr-vdUZO8VGOnjRehGHBXkSzchr37SYF2GNdQFYSp72jh5QUhzG";
+export const clientSecret = "EMnAWe06ioGtouJs7gLYT9chK9-2jJ--7MKRXpI8FesmY_2Kp-d_7aCqff7M9moEJBvuXoBO4clKtY0v";
